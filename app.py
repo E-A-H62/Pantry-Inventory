1bf38938fe7933605f6d3e3518f4f0e376a9db9a
William Barnett <will892@LAPTOP-GCQMH64J>
2024-07-09 14:08:44 -0700

    Fixes to bugs and basic implementation for user action

--- app.py
+++ app.py
@@ -2,59 +2,62 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify, f
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('home.html')
 
-@app.route('/inventory', methods=['GET', 'POST'])
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/inventory", methods=["GET", "POST"])
 def inventory():
     # fetch pantry items
-    pantry_items = [] # temp
+    pantry_items = ["water", "milk"]  # temp
 
-    if request.method == 'POST':
-        item = request.form['item']
+    if request.method == "POST":
+        item = request.form["item"]
 
-        if not item:
-            flash('Invalid input for item name', "danger")
-            return redirect(url_for('inventory'))
-        
+        print(item)
+
         # get function call to retrieve pricing from name primary key
-        price = 0 # temp
+        price = 1  # temp
 
         if price:
-            redirect(url_for('add', item = item))
-        
-        redirect(url_for('edit', item = item))
+            return redirect(url_for("add", item=item))
 
-    return render_template('inventory.html', pantry_items=pantry_items)
+        return redirect(url_for("edit", item=item))
 
-@app.route('/add/<item>', methods=['GET', 'POST'])
+    return render_template("inventory.html", pantry_items=pantry_items)
+
+
+@app.route("/add/<item>", methods=["GET", "POST"])
 def add(item):
-    if request.method == 'POST':
-        quantity = request.form['quantity']
-        price = request.form['price']
+    if request.method == "POST":
+        quantity = request.form["quantity"]
+        price = request.form["price"]
 
         # add item to database
+        print(quantity, price)
 
-        return redirect(url_for('add'), item = item)
-    
-    return render_template('add_inventory.html', item = item)
-    
+        return redirect(url_for("inventory"))
+
+    return render_template("add_inventory.html", item=item)
+
 
 # needs work to become fully functional
-@app.route('/edit/<item>', methods=['GET', 'POST'])
+@app.route("/edit/<int:item>", methods=["GET", "POST"])
 def edit(item):
-    if request.method == 'POST':
-        added = request.form['added']
-        removed = request.form['removed']
-        price = request.form['price']
+    if request.method == "POST":
+        added = request.form["added"]
+        removed = request.form["removed"]
+        price = request.form["price"]
 
         # update item in database
+        print(added, removed, price)
+        print(item)
+        return redirect(url_for("inventory"))
 
-        return redirect(url_for('edit'), item = item)
-    
-    return render_template('edit_inventory.html', item = item)
-    
+    return render_template("edit_inventory.html", item=item)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
