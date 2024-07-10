efabc11951425929bd4fc6fa5bc36bac2ec813a3
anthonyrodmir <tony04rod@gmail.com>
2024-07-11 03:52:24 +0900

    Addressed review comments

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
-import requests
+from recipes_api import get_recipes_from_api
 
 app = Flask(__name__)
-API_KEY = "db449bea6c7a42b1b91ea196b0cd41b1"
 
 @app.route("/")
 def home():
@@ -59,28 +58,11 @@ def edit(item):
 
     return render_template("edit_inventory.html", item=item)
 
-@app.route("/recipes", methods=["GET"])
+@app.route("/recipes", methods=["GET", "POST"])
 def recipes():
     ingredients = 'apples,flour,sugar'  # Example ingredients
     recipe_data = get_recipes_from_api(ingredients)
     return render_template("recipes.html", recipes=recipe_data)
-
-def get_recipes_from_api(ingredients):
-    api_url = "https://api.spoonacular.com/recipes/findByIngredients"
-    api_key = API_KEY
-    params = {
-        'apiKey': api_key,
-        'ingredients': ingredients,
-        'number': 4,  # Number of recipes to return
-        'ranking': 1, # change to 2 if want to minimize missing ingredients
-        'ignorePantry': True
-    }
-    response = requests.get(api_url, params=params)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f"Error: {response.status_code}")
-        return []
 
 if __name__ == "__main__":
     app.run(debug=True)
